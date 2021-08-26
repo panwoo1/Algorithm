54d70d10e0eea062ba446c5e1fe342f812d99747
panwoo1 <66258691+panwoo1@users.noreply.github.com>
2021-08-26 18:53:29 +0900

    #MoM algorithm

--- selection/Median_of_medians.py
+++ selection/Median_of_medians.py
@@ -1,26 +1,49 @@
-# pseudo code
-def MoM(A, k):
-    if ( | A | == 1):
-        return A[0]
-    S, L, M, medians = [], [], [], []
+def find_median_five(A):
+	S, M, L = [], [], []
+	p = A[0]
+	for a in A:
+		if(a < p):
+			S.append(a)
+		elif(a == p):
+			M.append(a)
+		else:
+			L.append(a)
+	
+	if(len(S) >= len(A)/2):
+		return find_median_five(S)
+	elif(len(S) + len(M) < len(A)/2):
+		return find_median_five(L)
+	else:
+		return p
+			
+	
+def MoM(A, k): # L의 값 중에서 k번째로 작은 수 리턴
+	if len(A) == 1: # no more recursion
+		return A[0]
+	i = 0
+	S, M, L, medians = [], [], [], []
+	while i+4 < len(A):
+		medians.append(find_median_five(A[i: i+5]))
+		i += 5
+		
+	if i < len(A) and i+4 >= len(A): # 마지막 그룹으로 5개 미만의 값으로 구성
+		medians.append(find_median_five(A[i:]))
 
-    i = 0
-    while(i + 4 < |A | ):
-        medians.append(find_median_five(L[i: i+5]))
-        i += 5
-    if(i < |A | and i + 4 >= |A |):
-        medians.append(find_median_five(L[i+5:]))
-    mom = MoM(medians, len(medians)//2)
-    for v in A:
-        if(v < mom):
-            S.append(v)
-        elif(v > mom):
-            L.append(v)
-        else:
-            M.append(v)
-    if(len(S) > k):
-        return MoM(S, k)
-    elif(len(S) + len(M) < k):
-        return MoM(B, k - len(S) - len(M))
-    else:
-        return mom
+	mom = MoM(medians, len(medians)//2)
+	for v in A:
+		if v < mom:
+			S.append(v)
+		elif v > mom:
+			L.append(v)
+		else:
+			M.append(v)
+
+	if (len(S) >= k): return MoM(S, k)
+	elif(len(S) + len(M) < k) : return MoM(L, k - len(S) - len(M))
+	else: return mom
+
+# n과 k를 입력의 첫 줄에서 읽어들인다
+# n개의 정수를 읽어들인다. (split 이용 + int로 변환)
+n, k = map(int, input().split())
+A = list(map(int, input().split()))
+print(MoM(A, k))
